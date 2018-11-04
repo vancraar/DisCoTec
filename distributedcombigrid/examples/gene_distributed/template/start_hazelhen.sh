fa592ec5941ca32d5cb94935eedb55a05256c26d
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2018-11-04 15:03:28 +0100

    corrected aprun node count

--- distributedcombigrid/examples/gene_distributed/template/start_hazelhen.sh
+++ distributedcombigrid/examples/gene_distributed/template/start_hazelhen.sh
@@ -1,2 +1,2 @@
 export LD_LIBRARY_PATH=/lustre/cray/ws8/ws/ipvpolli-EXAHD/combi/glpk/lib:/lustre/cray/ws8/ws/ipvpolli-EXAHD/combi/lib/sgpp:/lustre/cray/ws8/ws/ipvpolli-EXAHD/combi/distributedcombigrid/examples/gene_distributed/lib:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
-aprun -n 32768 ./gene_new_machine :  -n 1 ./manager -j2
+aprun -n 1366 -N 24 ./gene_new_machine :  -n 1 ./manager
